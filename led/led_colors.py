1c4cf27c6b9bc1b9a2fbff3e52a3cd17392fba71
RickHuynh <33076162+RickHuynh@users.noreply.github.com>
2018-12-18 15:31:09 -0800

    Revert "Ricks big branch"

--- led/led_colors.py
+++ led/led_colors.py
@@ -9,12 +9,6 @@ YELLOW = Adafruit_WS2801.RGB_to_color(255, 84, 3)
 DARK_TAN = Adafruit_WS2801.RGB_to_color(160, 90, 30)
 LIGHT_TAN = Adafruit_WS2801.RGB_to_color(235, 160, 95)
 DARK_PURPLE = Adafruit_WS2801.RGB_to_color(50, 30, 75)
-<<<<<<< HEAD
-LIGHT_BLUE = Adafruit_WS2801.RGB_to_color(75, 180, 230)
-WHITE = Adafruit_WS2801.RGB_to_color(255,255,255)
-BLACK = Adafruit_WS2801.RGB_to_color(0,0,0)
-=======
 ORANGE = Adafruit_WS2801.RGB_to_color(236, 92, 24)
 LIGHT_BLUE = Adafruit_WS2801.RGB_to_color(75, 180, 230)
 LIGHT_PURPLE = Adafruit_WS2801.RGB_to_color(213, 43, 255)
->>>>>>> 13b9349cf80cfe0314d2126eed7c5cd4fbb88e13
