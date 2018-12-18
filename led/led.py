3be7afe3588082a0c21d4da0d4061de5fcaf5885
Will <wlxu@ucsd.edu>
2018-12-18 08:19:51 +0000

    add colors

--- led/led.py
+++ led/led.py
@@ -16,6 +16,9 @@ LIGHT_RED = Adafruit_WS2801.RGB_to_color(255, 0, 0)
 DARK_GREEN = Adafruit_WS2801.RGB_to_color(0, 128, 0)
 LIGHT_GREEN = Adafruit_WS2801.RGB_to_color(0, 255, 0)
 YELLOW = Adafruit_WS2801.RGB_to_color(255, 84, 3)
+DARK_TAN = Adafruit_WS2801.RGB_to_color(160, 90, 30)
+LIGHT_TAN = Adafruit_WS2801.RGB_to_color(235, 160, 95)
+DARK_PURPLE = Adafruit_WS2801.RGB_to_color(50, 30, 75)
 
 # Dimensions of a letter.
 LETTER_HEIGHT = 7
