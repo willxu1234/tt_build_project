4ef13d9725948df5f11ca00fd528048ae419bf7e
zhaoesther <ewzhao@ucsd.edu>
2018-12-17 18:46:57 -0800

    Theta Tau scrolling colon

--- led/led.py
+++ led/led.py
@@ -174,8 +174,8 @@ def draw_theta(start, color):
         
         ar_row = [2,5,8]
         # column 3 to 4
-		for row in range(len(ar_row)):
-            for col in range(3,6)
+	for row in range(len(ar_row)):
+            for col in range(3,6):
                 front[row][start + col] = color
             
 
