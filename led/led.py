56b312ad64fd2535ab0526df5a1888510bdb3990
Will <wlxu@ucsd.edu>
2018-12-18 23:20:36 +0000

    moved to m and changed readme

--- led/led.py
+++ led/led.py
@@ -214,7 +214,7 @@ def draw_message(pixels, message, message_color, background_color, wait=0.5):
 			draw_led.draw_plant_left(start * FULL_LETTER, front) 
 		elif letter == 'c': 
 			draw_led.draw_plant_right(start * FULL_LETTER, front) 
-		elif letter == 'e': 
+		elif letter == 'm': 
 			draw_led.draw_dna_left(start * FULL_LETTER, front)
 		elif letter == 'g':
 			draw_led.draw_dna_right(start * FULL_LETTER, front) 
@@ -222,9 +222,9 @@ def draw_message(pixels, message, message_color, background_color, wait=0.5):
 			draw_led.draw_build_left(start * FULL_LETTER, front)
 		elif letter == 'k': 
 			draw_led.draw_build_right(start * FULL_LETTER, front) 
-		elif letter == 'o': # alt + 4
+		elif letter == 'o':
 			draw_led.draw_zero(start * FULL_LETTER, LIGHT_GREEN, front)
-		elif letter == 'l': # alt + 1
+		elif letter == 'l':
 			draw_led.draw_one(start * FULL_LETTER, LIGHT_GREEN, front)
 		elif letter == ' ':
 			# Draw a full space.
@@ -251,4 +251,4 @@ if __name__ == "__main__":
 	while True:
 		# += are reserved chars for Theta Tau symbols
 		# draw_message(pixels, '<>+=;:|e^()', DARK_RED, YELLOW, 0.06)
-		draw_message(pixels, '<>+=;:|e^() ololollllololollllololloolllo', DARK_RED, Adafruit_WS2801.RGB_to_color(0, 0, 0), 0.06)
+		draw_message(pixels, 'acmgik', DARK_RED, Adafruit_WS2801.RGB_to_color(0, 0, 0), 0.06)
