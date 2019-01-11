fb57596d7005b9a0abef2c846898ee1bd8d30490
willxu1234 <wlxu@ucsd.edu>
2019-01-10 17:15:06 -0800

    create hashmap with functions

--- led/draw_led.py
+++ led/draw_led.py
@@ -331,6 +331,9 @@ def draw_zero(start, color, front):
 	front[5][start + 3] = color
 	front[4][start + 4] = color
 
+def draw_bin_zero(start, color, front):
+	draw_zero(start, LIGHT_GREEN, front)
+
 def draw_one(start, color, front):
 	front[3][start + 2] = color
 	for row in range(2, 8):
@@ -338,6 +341,9 @@ def draw_one(start, color, front):
 
 	for col in range(start + 2, start + 5):
 		front[8][col] = color
+
+def draw_bin_one(start, color, front):
+	draw_one(start, LIGHT_GREEN, front)
 
 def draw_two(start, color, front):
 	for row in range(2, 6):
@@ -479,7 +485,7 @@ def draw_delta(start, color, front):
 	front[2][start + 3] = color
 
 
-def draw_rose_left(start, front):
+def draw_rose_left(start, color, front):
 	front[3][start + 2] = DARK_RED
 	front[4][start + 2] = DARK_RED
 
@@ -503,7 +509,7 @@ def draw_rose_left(start, front):
 	front[7][start + 6] = LIGHT_GREEN
 	front[8][start + 6] = DARK_GREEN
 
-def draw_reverse_rose_right(start, front):
+def draw_reverse_rose_right(start, color, front):
 	front[3][start + 4] = DARK_RED
 	front[4][start + 4] = DARK_RED
 
@@ -527,7 +533,7 @@ def draw_reverse_rose_right(start, front):
 	front[7][start] = LIGHT_GREEN
 	front[8][start] = DARK_GREEN
 
-def draw_rose_right(start, front):
+def draw_rose_right(start, color, front):
 	front[4][start] = LIGHT_GREEN
 	front[5][start] = LIGHT_GREEN
 	front[6][start] = LIGHT_GREEN
@@ -542,7 +548,7 @@ def draw_rose_right(start, front):
 
 	front[8][start + 4] = DARK_GREEN
 
-def draw_reverse_rose_left(start, front):
+def draw_reverse_rose_left(start, color, front):
 	front[8][start + 2] = DARK_GREEN
 
 	front[8][start + 3] = DARK_GREEN
@@ -557,7 +563,7 @@ def draw_reverse_rose_left(start, front):
 	front[5][start + 6] = LIGHT_GREEN
 	front[6][start + 6] = LIGHT_GREEN
 
-def draw_ht_left(start, front):
+def draw_ht_left(start, color, front):
 	front[2][start + 3] = DARK_PURPLE
 	front[3][start + 3] = DARK_PURPLE
 
@@ -572,7 +578,7 @@ def draw_ht_left(start, front):
 	front[4][start + 6] = DARK_PURPLE
 	front[7][start + 6] = DARK_PURPLE
 
-def draw_ht_mid(start, front):
+def draw_ht_mid(start, color, front):
 	front[5][start] = DARK_PURPLE
 	front[7][start] = DARK_PURPLE
 	front[5][start + 6] = DARK_PURPLE
@@ -593,7 +599,7 @@ def draw_ht_mid(start, front):
 	for col in range(col + 2, col + 5):
 		front[8][col] = DARK_TAN
 
-def draw_ht_right(start, front):
+def draw_ht_right(start, color, front):
 	front[2][start] = DARK_PURPLE
 	front[4][start] = DARK_PURPLE
 	front[7][start] = DARK_PURPLE
@@ -610,7 +616,7 @@ def draw_ht_right(start, front):
 	for row in range(3, 6):
 		front[row][start + 3] = DARK_PURPLE
 
-def draw_plant_left(start, front):
+def draw_plant_left(start, color, front):
 	front[3][start + 5] = LIGHT_GREEN
 	front[3][start + 6] = LIGHT_GREEN
 	front[4][start + 4] = LIGHT_GREEN
@@ -620,7 +626,7 @@ def draw_plant_left(start, front):
 	front[8][start + 5] = ORANGE
 	front[8][start + 6] = ORANGE
 
-def draw_plant_right(start, front):
+def draw_plant_right(start, color, front):
 	front[3][start + 1] = LIGHT_GREEN
 	front[3][start + 2] = LIGHT_GREEN
 	front[4][start] = LIGHT_GREEN
@@ -635,7 +641,7 @@ def draw_plant_right(start, front):
 	front[8][start + 2] = ORANGE
 	front[8][start + 3] = ORANGE
 
-def draw_dna_left(start, front):
+def draw_dna_left(start, color, front):
 	front[2][start + 2] = LIGHT_BLUE
 	front[2][start + 3] = LIGHT_BLUE
 	front[3][start + 2] = YELLOW
@@ -653,7 +659,7 @@ def draw_dna_left(start, front):
 	front[7][start + 2] = LIGHT_PURPLE
 	front[7][start + 3] = LIGHT_PURPLE
 
-def draw_dna_right(start, front):
+def draw_dna_right(start, color, front):
 	front[2][start + 3] = LIGHT_PURPLE
 	front[2][start + 4] = LIGHT_PURPLE
 	front[3][start + 1] = LIGHT_PURPLE
@@ -671,7 +677,7 @@ def draw_dna_right(start, front):
 	front[7][start + 3] = LIGHT_BLUE
 	front[7][start + 4] = LIGHT_BLUE
 
-def draw_build_left(start, front):
+def draw_build_left(start, color, front):
 	for col in range(start + 2, start + 7):
 		front[3][col] = DARK_PURPLE
 		front[5][col] = DARK_PURPLE
@@ -692,7 +698,7 @@ def draw_build_left(start, front):
 	front[8][start + 3] = YELLOW
 	front[8][start + 5] = YELLOW
 
-def draw_build_right(start, front):
+def draw_build_right(start, color, front):
 	front[5][start + 1] = DARK_PURPLE
 	front[5][start + 3] = DARK_PURPLE
 	for col in range(start, start + 5):
@@ -704,7 +710,7 @@ def draw_build_right(start, front):
 	front[8][start + 2] = YELLOW
 	front[8][start + 3] = YELLOW
 
-def draw_potion(start, front):
+def draw_potion(start, color, front):
 	front[2][start + 5] = LIGHT_BLUE
 	front[2][start + 6] = LIGHT_BLUE
 	front[3][start + 6] = LIGHT_BLUE
@@ -720,7 +726,7 @@ def draw_potion(start, front):
 	front[8][start + 5] = LIGHT_BLUE
 	front[8][start + 6] = LIGHT_BLUE
 
-def draw_potion2(start, front):
+def draw_potion2(start, color, front):
 	front[2][start + 1] = LIGHT_BLUE
 	front[2][start] = LIGHT_BLUE
 	front[3][start] = LIGHT_BLUE
@@ -736,7 +742,7 @@ def draw_potion2(start, front):
 	front[8][start + 1] = LIGHT_BLUE
 	front[8][start] = LIGHT_BLUE
 
-def draw_rocket(start, front):
+def draw_rocket(start, color, front):
 	front[4][start + 4] = DARK_RED
 	front[4][start + 5] = DARK_RED
 	front[4][start + 6] = DARK_RED
@@ -751,7 +757,7 @@ def draw_rocket(start, front):
 	front[6][start + 5] = DARK_RED
 	front[6][start + 6] = DARK_RED
 
-def draw_rocket2(start, front):
+def draw_rocket2(start, color, front):
 	front[3][start + 3] = DARK_RED
 	front[3][start + 4] = DARK_RED
 
@@ -774,3 +780,68 @@ def draw_rocket2(start, front):
 	front[7][start + 3] = DARK_RED
 	front[7][start + 4] = DARK_RED
 	front[7][start + 4] = DARK_RED
+
+# Another constant hashmap that maps letters/encodings to functions.
+DRAW_FN = {
+	'R': draw_R(),
+	'B': draw_B(),
+	'D': draw_D(),
+	'F': draw_F(),
+	'I': draw_I(),
+	'K': draw_K(),
+	'A': draw_A(),
+	'C': draw_C(),
+	'E': draw_E(),
+	'e': draw_epsilon(),
+	'U': draw_U(),
+	'S': draw_S(),
+	'H': draw_H(),
+	'0': draw_zero(),
+	'1': draw_one(),
+	'2': draw_two(),
+	'3': draw_three(),
+	'4': draw_four(),
+	'5': draw_five(),
+	'6': draw_six(),
+	'7': draw_seven(),
+	'8': draw_eight(),
+	'9': draw_nine(),
+	'!': draw_exclamation(),
+	'.': draw_period(),
+	'+': draw_theta(),
+	'=': draw_tau(),
+	'^': draw_delta(),
+	'M': draw_M(),
+	'O': draw_O(),
+	'V': draw_V(),
+	'Z': draw_Z(),
+	'G': draw_G(),
+	'J': draw_J(),
+	'L': draw_L(),
+	'N': draw_N(),
+	'P': draw_P(),
+	'T': draw_T(),
+	'W': draw_W(),
+	'Y': draw_Y(),
+	'Q': draw_Q(),
+	'X': draw_X(),
+	'<': draw_rose_left()
+	'>': draw_rose_right()
+	'(': draw_reverse_rose_left()
+	')': draw_reverse_rose_right()
+	';': draw_ht_left()
+	':': draw_ht_mid()
+	'|': draw_ht_right()
+	'a': draw_plant_left()
+	'c': draw_plant_right()
+	'm': draw_dna_left()
+	'g': draw_dna_right()
+	'i': draw_build_left()
+	'k': draw_build_right()
+	'b': draw_rocket()
+	'd': draw_rocket2()
+	'f': draw_potion()
+	'h': draw_potion2()
+	'o': draw_bin_zero()
+	'l': draw_bin_one()
+}
--- led/led.py
+++ led/led.py
@@ -123,7 +123,16 @@ def draw_letters(pixels, color, background_color, wait=0.5):
 # Draws the message in message_color with a background color. rainbow decides whether or not the back
 # will be sweep through the rainbow colors.
 def draw_message(pixels, message, message_color, background_color, rainbow=True, wait=0.5):
-	message_col_count = FULL_LETTER * len(message)
+	# message_col_count = FULL_LETTER * len(message)
+	message_col_count = 0
+	for letter in message:
+		# If the letter is in the hashmap defined in draw_led, make the front
+		# matrix that much bigger. Otherwise, just assume the maximum letter
+		# width.
+		if letter in draw_led.LETTER_WIDTHS:
+			message_col_count += draw_led.LETTER_WIDTHS[letter]
+		else:
+			message_col_count += FULL_LETTER
 	global front
 	if PIXEL_COL < message_col_count:
 		del front[:]
@@ -133,136 +142,23 @@ def draw_message(pixels, message, message_color, background_color, rainbow=True,
 			for c in range(message_col_count + 2 * PIXEL_COL):
 				row.append(background_color)
 
-	for start in range(1, len(message) + 1):
-		letter = message[start - 1]
-		if letter == 'R':
-			draw_led.draw_R(start * FULL_LETTER, message_color, front)
-		elif letter == 'B':
-			draw_led.draw_B(start * FULL_LETTER, message_color, front)
-		elif letter == 'D':
-			draw_led.draw_D(start * FULL_LETTER, message_color, front)
-		elif letter == 'F':
-			draw_led.draw_F(start * FULL_LETTER, message_color, front)
-		elif letter == 'I':
-			draw_led.draw_I(start * FULL_LETTER, message_color, front)
-		elif letter == 'K':
-			draw_led.draw_K(start * FULL_LETTER, message_color, front)
-		elif letter == 'A':
-			draw_led.draw_A(start * FULL_LETTER, message_color, front)
-		elif letter == 'C':
-			draw_led.draw_C(start * FULL_LETTER, message_color, front)
-		elif letter == 'E':
-			draw_led.draw_E(start * FULL_LETTER, message_color, front)
-		elif letter == 'e':
-			draw_led.draw_epsilon(start * FULL_LETTER, message_color, front)
-		elif letter == 'U':
-			draw_led.draw_U(start * FULL_LETTER, message_color, front)
-		elif letter == 'S':
-			draw_led.draw_S(start * FULL_LETTER, message_color, front)
-		elif letter == 'H':
-			draw_led.draw_H(start * FULL_LETTER, message_color, front)
-		elif letter == '0':
-			draw_led.draw_zero(start * FULL_LETTER, message_color, front)
-		elif letter == '1':
-			draw_led.draw_one(start * FULL_LETTER, message_color, front)
-		elif letter == '2':
-			draw_led.draw_two(start * FULL_LETTER, message_color, front)
-		elif letter == '3':
-			draw_led.draw_three(start * FULL_LETTER, message_color, front)
-		elif letter == '4':
-			draw_led.draw_four(start * FULL_LETTER, message_color, front)
-		elif letter == '5':
-			draw_led.draw_five(start * FULL_LETTER, message_color, front)
-		elif letter == '6':
-			draw_led.draw_six(start * FULL_LETTER, message_color, front)
-		elif letter == '7':
-			draw_led.draw_seven(start * FULL_LETTER, message_color, front)
-		elif letter == '8':
-			draw_led.draw_eight(start * FULL_LETTER, message_color, front)
-		elif letter == '9':
-			draw_led.draw_nine(start * FULL_LETTER, message_color, front)
-		elif letter == '!':
-			draw_led.draw_exclamation(start * FULL_LETTER, message_color, front)
-		elif letter == '.':
-			draw_led.draw_period(start * FULL_LETTER, message_color, front)
-		elif letter == '+':
-			draw_led.draw_theta(start * FULL_LETTER, message_color, front)
-		elif letter == '=':
-			draw_led.draw_tau(start * FULL_LETTER, message_color, front)
-		elif letter == '^':
-			draw_led.draw_delta(start * FULL_LETTER, message_color, front)
-		elif letter == 'M':
-			draw_led.draw_M(start * FULL_LETTER, message_color, front)
-		elif letter == 'O':
-			draw_led.draw_O(start * FULL_LETTER, message_color, front)
-		elif letter == 'V':
-			draw_led.draw_V(start * FULL_LETTER, message_color, front)
-		elif letter == 'Z':
-			draw_led.draw_Z(start * FULL_LETTER, message_color, front)
-		elif letter == 'G':
-			draw_led.draw_G(start * FULL_LETTER, message_color, front)
-		elif letter == 'J':
-			draw_led.draw_J(start * FULL_LETTER, message_color, front)
-		elif letter == 'L':
-			draw_led.draw_L(start * FULL_LETTER, message_color, front)
-		elif letter == 'N':
-			draw_led.draw_N(start * FULL_LETTER, message_color, front)
-		elif letter == 'P':
-			draw_led.draw_P(start * FULL_LETTER, message_color, front)
-		elif letter == 'T':
-			draw_led.draw_T(start * FULL_LETTER, message_color, front)
-		elif letter == 'W':
-			draw_led.draw_W(start * FULL_LETTER, message_color, front)
-		elif letter == 'Y':
-			draw_led.draw_Y(start * FULL_LETTER, message_color, front)
-		elif letter == 'Q':
-			draw_led.draw_Q(start * FULL_LETTER, message_color, front)
-		elif letter == 'X':
-			draw_led.draw_X(start * FULL_LETTER, message_color, front)
-		elif letter == '<':
-			draw_led.draw_rose_left(start * FULL_LETTER, front)
-		elif letter == '>':
-			draw_led.draw_rose_right(start * FULL_LETTER, front)
-		elif letter == '(':
-			draw_led.draw_reverse_rose_left(start * FULL_LETTER, front)
-		elif letter == ')':
-			draw_led.draw_reverse_rose_right(start * FULL_LETTER, front)
-		elif letter == ';':
-			draw_led.draw_ht_left(start * FULL_LETTER, front)
-		elif letter == ':':
-			draw_led.draw_ht_mid(start * FULL_LETTER, front)
-		elif letter == '|':
-			draw_led.draw_ht_right(start * FULL_LETTER, front)
-		elif letter == 'a':
-			draw_led.draw_plant_left(start * FULL_LETTER, front)
-		elif letter == 'c':
-			draw_led.draw_plant_right(start * FULL_LETTER, front)
-		elif letter == 'm':
-			draw_led.draw_dna_left(start * FULL_LETTER, front)
-		elif letter == 'g':
-			draw_led.draw_dna_right(start * FULL_LETTER, front)
-		elif letter == 'i':
-			draw_led.draw_build_left(start * FULL_LETTER, front)
-		elif letter == 'k':
-			draw_led.draw_build_right(start * FULL_LETTER, front)
-		elif letter == 'b':
-			draw_led.draw_rocket(start * FULL_LETTER, front)
-		elif letter == 'd':
-			draw_led.draw_rocket2(start * FULL_LETTER, front)
-		elif letter == 'f':
-			draw_led.draw_potion(start * FULL_LETTER, front)
-		elif letter == 'h':
-			draw_led.draw_potion2(start * FULL_LETTER, front)
-		elif letter == 'o':
-			draw_led.draw_zero(start * FULL_LETTER, LIGHT_GREEN, front)
-		elif letter == 'l':
-			draw_led.draw_one(start * FULL_LETTER, LIGHT_GREEN, front)
-		elif letter == ' ':
+	# index of the current letter
+	index = 0
+	while start < message_col_count:
+		if letter == ' ':
 			# Draw a full space.
 			pass
+		elif letter in draw_led.DRAW_FN:
+			draw_led.DRAW_FN[letter](start, message_color, front)
 		else:
 			print("Unable to draw that letter.")
 			pass
+
+		index += 1
+		if letter in draw_led.LETTER_WIDTHS:
+			start += draw_led.LETTER_WIDTHS[letter]
+		else:
+			start += FULL_LETTER
 
 	draw_scrolling(pixels, rainbow, wait)
 
@@ -300,7 +196,7 @@ if __name__ == "__main__":
 
 	# Display a rainbow display on the front for 10 seconds.
 	rainbow_front(pixels, wait_between=0.005, duration=10)
-	
+
 	no_ip = True
 
 	while True:
